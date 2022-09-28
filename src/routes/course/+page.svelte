362e3df30426a47888b68ea4a0c65c9dd69b4b89
SantriptaSharma <santripta@gmail.com>
2022-09-28 12:04:22 +0530

    fix page buttons transition scope

--- src/routes/course/+page.svelte
+++ src/routes/course/+page.svelte
@@ -95,7 +95,7 @@
         <div class = "md:col-span-3 2xl:col-span-4 justify-center flex gap-2 flex-wrap">
             {#each new Array(pageCount) as _, i (i)}
                 <button class = "px-4 py-2 rounded-xl bg-secondary hover:bg-indigo-700 transition-colors" class:bg-indigo-700 = {page === i} on:click = {() => page = i}
-                    transition:fly = {{duration: 200, easing: cubicInOut, y: -100}} animate:flip = {{duration: 400, easing: cubicInOut}}>
+                    transition:fly|local = {{duration: 200, easing: cubicInOut, y: -100}} animate:flip = {{duration: 400, easing: cubicInOut}}>
                     {i + 1}
                 </button>
             {/each}
@@ -129,7 +129,7 @@
         <div class = "md:col-span-3 2xl:col-span-4 justify-center flex gap-2 flex-wrap">
             {#each new Array(pageCount) as _, i (i)}
                 <button class = "px-4 py-2 rounded-xl bg-secondary hover:bg-indigo-700 transition-colors" class:bg-indigo-700 = {page === i} on:click = {() => page = i}
-                    transition:fly = {{duration: 200, easing: cubicInOut, y: -100}} animate:flip = {{duration: 400, easing: cubicInOut}}>
+                    transition:fly|local = {{duration: 200, easing: cubicInOut, y: -100}} animate:flip = {{duration: 400, easing: cubicInOut}}>
                     {i + 1}
                 </button>
             {/each}
